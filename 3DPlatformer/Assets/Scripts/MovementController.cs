0db7e432b1016670404365bbf50a1e82dde14c24
o4thbreaker <146327700+o4thbreaker@users.noreply.github.com>
2023-10-18 23:45:39 +0700

    add jump
    added jump. right now its pretty simple and works not as perfect as i wish to but its ok for now polish later

--- 3DPlatformer/Assets/Scripts/MovementController.cs
+++ 3DPlatformer/Assets/Scripts/MovementController.cs
@@ -1,19 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class MovementController : MonoBehaviour
 {
-    [SerializeField] private float playerSpeed = 1f;
+    [SerializeField] private float playerSpeed = 7f;
     [SerializeField] private float rotationSpeed = 1f;
+
+    [SerializeField] private float groundedGravity = -.05f;
+    [SerializeField] private float gravity = -9.8f;
 
     private PlayerInputActions playerInputActions;
     private CharacterController characterController;
 
     private Vector2 currentMovementInput; // wasd normalized ex W is (0,1)
     private Vector3 currentMovement; // player
+    private Vector3 currentCameraRealtiveMovement;
     private bool isMovementPressed;
+
+    private bool isJumpPressed = false;
+    private float initialJumpVelocity;
+    [SerializeField] private float maxJumpHeight = 1f;
+    [SerializeField] private float maxJumpTime = 0.5f;
+    private bool isJumping = false;
 
     private void Awake()
     {
@@ -23,6 +34,10 @@ public class MovementController : MonoBehaviour
         playerInputActions.Player.Move.started += OnMovementInput;
         playerInputActions.Player.Move.canceled += OnMovementInput;
         playerInputActions.Player.Move.performed += OnMovementInput;
+        playerInputActions.Player.Jump.started += OnJump;
+        playerInputActions.Player.Jump.canceled += OnJump;
+
+        SetupJumpVariables();
     }
 
     private void OnMovementInput(InputAction.CallbackContext context)
@@ -31,6 +46,34 @@ public class MovementController : MonoBehaviour
         currentMovement.x = currentMovementInput.x;
         currentMovement.z = currentMovementInput.y;
         isMovementPressed = currentMovementInput.x != 0 || currentMovementInput.y != 0;
+
+        float playerVerticalInput = currentMovement.z;
+        float playerHorizontalInput = currentMovement.x;
+
+        Vector3 cameraForward = Camera.main.transform.forward;
+        Vector3 cameraRight = Camera.main.transform.right;
+        cameraForward.y = 0;
+        cameraRight.y = 0;
+        cameraForward = cameraForward.normalized;
+        cameraRight = cameraRight.normalized;
+
+        Vector3 forwardRelativeVerticalInput = playerVerticalInput * cameraForward;
+        Vector3 rightRelativeHorizontalInput = playerHorizontalInput * cameraRight;
+
+        currentCameraRealtiveMovement = forwardRelativeVerticalInput + rightRelativeHorizontalInput;
+    }
+
+    private void SetupJumpVariables()
+    {
+        float timeToApex = maxJumpTime / 2; // apex is a highest point of parabola aka highest jump point 
+
+        gravity = (-2 * maxJumpHeight) / (timeToApex * timeToApex);
+        initialJumpVelocity = (2 * maxJumpHeight) / timeToApex;
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        isJumpPressed = context.ReadValueAsButton();
     }
 
     private void OnEnable()
@@ -58,29 +101,51 @@ public class MovementController : MonoBehaviour
         }
     }
 
-    private void HandleMovement()
+    private void HandleGravity()
     {
-        float playerVerticalInput = currentMovement.z;
-        float playerHorizontalInput = currentMovement.x;
+        bool isFalling = currentCameraRealtiveMovement.y <= 0.0f || !isJumpPressed;
+        float fallMultiplier = 2.0f;
 
-        Vector3 cameraForward = Camera.main.transform.forward;
-        Vector3 cameraRight = Camera.main.transform.right;
-        cameraForward.y = 0;
-        cameraRight.y = 0;
-        cameraForward = cameraForward.normalized;
-        cameraRight = cameraRight.normalized;
+        if (characterController.isGrounded)
+        {
+            currentCameraRealtiveMovement.y = groundedGravity;
+        }
+        else if (isFalling)
+        {
+            float previousYVelocity = currentCameraRealtiveMovement.y;
+            float newYVelocity = currentCameraRealtiveMovement.y + (gravity * fallMultiplier * Time.deltaTime);
+            float nextYVelocity = (previousYVelocity + newYVelocity) * .5f;
+            currentCameraRealtiveMovement.y = nextYVelocity;
+        }
+        else
+        {
+            float previousYVelocity = currentCameraRealtiveMovement.y;
+            float newYVelocity = currentCameraRealtiveMovement.y + (gravity * Time.deltaTime);
+            float nextYVelocity = (previousYVelocity + newYVelocity) * .5f;
+            currentCameraRealtiveMovement.y = nextYVelocity;
+        }
+    }
 
-        Vector3 forwardRelativeVerticalInput = playerVerticalInput * cameraForward;
-        Vector3 rightRelativeHorizontalInput = playerHorizontalInput * cameraRight;
-
-        Vector3 cameraRealtiveMovement = forwardRelativeVerticalInput + rightRelativeHorizontalInput;
-
-        characterController.Move(cameraRealtiveMovement * Time.deltaTime * playerSpeed);
+    private void HandleJump()
+    {
+        if (!isJumping && characterController.isGrounded && isJumpPressed)
+        { 
+            isJumping = true;
+            currentCameraRealtiveMovement.y = initialJumpVelocity * .5f;
+        }
+        else if (isJumping && characterController.isGrounded && !isJumpPressed)
+        {
+            isJumping = false;
+        }
     }
 
     private void Update()
     {
         HandleRotation();
-        HandleMovement();
+
+        characterController.Move(currentCameraRealtiveMovement * Time.deltaTime * playerSpeed);
+
+        HandleGravity();
+        HandleJump(); 
     }
 }
